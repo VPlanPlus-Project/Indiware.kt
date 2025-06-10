5a7efd029de096840f8f9b9ae8b778f13364fe22
Julius Vincent Babies <julius@familie-babies.de>
2025-06-10 07:37:20 +0200

    #3 Gitlab

--- src/commonTest/kotlin/plus/vplan/lib/indiware/Test.kt
+++ src/commonTest/kotlin/plus/vplan/lib/indiware/Test.kt
@@ -3,8 +3,6 @@ package plus.vplan.lib.indiware
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import kotlinx.coroutines.runBlocking
-import kotlinx.datetime.LocalDate
-import kotlinx.serialization.json.Json
 import plus.vplan.lib.indiware.source.Authentication
 import plus.vplan.lib.indiware.source.IndiwareClient
 import plus.vplan.lib.indiware.source.Response
@@ -12,24 +10,6 @@ import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class CommonGreetingTest {
-
-    @Test
-    fun testExample() = runBlocking {
-        val client = HttpClient(CIO)
-        val indiwareClient = IndiwareClient(
-            authentication = Authentication(
-                indiwareSchoolId = "10000000",
-                username = "schueler",
-                password = "123123"
-            ),
-            client = client
-        )
-        val result = indiwareClient.getSubstitutionPlan(LocalDate(2025, 5, 26))
-        assertTrue(result is Response.Success)
-        println(result.data)
-        println(Json.encodeToString(result.data.classes.first().lessons.first()))
-    }
-
     @Test
     fun exampleMobdaten() {
         val client = HttpClient(CIO)
